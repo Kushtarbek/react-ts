a79de725eb3c811bf7384b702fcb732f40eaa666
Kushtarbek <dr.kushtarbek@gmail.com>
2021-01-06 23:06:42 -0600

    HOCs with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import Message from "./Message";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   //let aTuple: [string, number] = ["text ", 124];
+
+//   // enum weekdays {
+//   //   first = 1,
+//   //   sec = 2,
+//   // }
+
+//   // //any
+//   // let firstName: any = 123;
+
+//   // //void
+//   // const warning = (): void => {
+//   //   console.log("Warning!");
+//   // };
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <Message message="this is prop" />
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+import Message from "./Message";
+
+const initialState = {
+  name: "Manny2",
+  message: "TypeScript is cool",
+};
+
+type State = Readonly<typeof initialState>;
+class App extends Component<any, State> {
+  readonly state: State = initialState;
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Message name={this.state.name} message={this.state.message} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import messageHoc from "./HOC";
+
+const example = (props: any): any => (
+  <p>
+    {props.name}, {props.message}
+  </p>
+);
+
+const Message = messageHoc(example);
+
+export default Message;
